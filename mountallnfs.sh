a6b0be8e3fad248d7b35ecf2c94620504eaeef1d
Faisal <faisal.is@gmail.com>
2015-11-23 14:49:28 +0530

    Add support for rancher configured /opt/data

--- mountallnfs.sh
+++ mountallnfs.sh
@@ -4,6 +4,10 @@ if [ -f /data/fstab.nfs ]; then
     cat /data/fstab.nfs >> /etc/fstab
 fi
 
+if [ -f /opt/data/fstab.nfs ]; then
+    cat /opt/data/fstab.nfs >> /etc/fstab
+fi
+
 mount -a -t nfs
 mount -a -t nfs4
 
